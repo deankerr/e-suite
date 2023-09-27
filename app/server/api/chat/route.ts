7f5df51323a700dd05a0d53550b89cc2238d8cf1
Dean Kerr <dean.kerr@gmail.com>
2023-09-28 02:45:44 +1000

    add moderation endpoint

--- app/server/api/chat/route.ts
+++ app/server/api/chat/route.ts
@@ -1,14 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 import OpenAI from 'openai'
-import {z} from 'zod'
+import { z } from 'zod'
 
 export async function POST(request: NextRequest) {
-  const {provider, ...params} = requestSchema.parse(await request.json())
-  console.log('req:', params)
-  
+  console.log('chat')
+  const { provider, ...params } = requestSchema.parse(await request.json())
+  console.log('params:', params)
+
   let message: string | undefined
   if (provider === 'openai') message = await openai(params)
-  if (provider ===  'openrouter') message = await openrouter(params)
+  if (provider === 'openrouter') message = await openrouter(params)
   if (!message) throw new Error('provider/response error')
 
   return NextResponse.json(message)
@@ -24,11 +25,11 @@ async function openai(params: ChatParams) {
 
 async function openrouter(params: ChatParams) {
   const api = new OpenAI({
-    apiKey: process.env.OPENROUTER_API_KEY, 
-    baseURL: 'https://openrouter.ai/api/v1/', 
+    apiKey: process.env.OPENROUTER_API_KEY,
+    baseURL: 'https://openrouter.ai/api/v1/',
     defaultHeaders: {
       'HTTP-Referer': process.env.SITE_URL,
-    }
+    },
   })
   const request = await api.chat.completions.create(params)
   const message = request.choices[0].message.content
@@ -37,13 +38,15 @@ async function openrouter(params: ChatParams) {
 }
 
 const requestSchema = z.object({
-  messages: z.array(z.object({
-    role: z.enum(['system', 'user', 'assistant']),
-    name: z.optional(z.string()),
-    content: z.string()
-  })),
+  messages: z.array(
+    z.object({
+      role: z.enum(['system', 'user', 'assistant']),
+      name: z.optional(z.string()),
+      content: z.string(),
+    }),
+  ),
   model: z.string(),
-  provider: z.string()
+  provider: z.string(),
 })
 
 type RequestSchema = z.infer<typeof requestSchema>
---
+++ app/server/api/moderation/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from 'next/server'
+import OpenAI from 'openai'
+import { z } from 'zod'
+
+export async function POST(request: NextRequest) {
+  console.log('moderation')
+  const params = requestSchema.parse(await request.json())
+  console.log('params', params)
+
+  const api = new OpenAI()
+  const response = await api.moderations.create(params)
+  return NextResponse.json(response)
+}
+
+const requestSchema = z.object({
+  input: z.string().or(z.string().array()),
+  model: z.optional(z.enum(['text-moderation-stable', 'text-moderation-latest'])),
+})
