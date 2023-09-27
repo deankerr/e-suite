33329659a79642c5f87f0beccc220cbfd6fe8c92
Dean Kerr <dean.kerr@gmail.com>
2023-09-28 01:00:12 +1000

    add OpenRouter

--- app/server/api/chat/route.ts
+++ app/server/api/chat/route.ts
@@ -3,17 +3,36 @@ import OpenAI from 'openai'
 import {z} from 'zod'
 
 export async function POST(request: NextRequest) {
-  const params = requestSchema.parse(await request.json())
+  const {provider, ...params} = requestSchema.parse(await request.json())
   console.log('req:', params)
-  const message = await openai(params)
+  
+  let message: string | undefined
+  if (provider === 'openai') message = await openai(params)
+  if (provider ===  'openrouter') message = await openrouter(params)
+  if (!message) throw new Error('provider/response error')
+
   return NextResponse.json(message)
 }
 
-async function openai(params: RequestSchema) {
+async function openai(params: ChatParams) {
   const api = new OpenAI()
   const request = await api.chat.completions.create(params)
   const message = request.choices[0].message.content
   if (!message) throw new Error('openai response error')
+  return message
+}
+
+async function openrouter(params: ChatParams) {
+  const api = new OpenAI({
+    apiKey: process.env.OPENROUTER_API_KEY, 
+    baseURL: 'https://openrouter.ai/api/v1/', 
+    defaultHeaders: {
+      'HTTP-Referer': process.env.SITE_URL,
+    }
+  })
+  const request = await api.chat.completions.create(params)
+  const message = request.choices[0].message.content
+  if (!message) throw new Error('openrouter response error')
   return message
 }
 
@@ -23,7 +42,9 @@ const requestSchema = z.object({
     name: z.optional(z.string()),
     content: z.string()
   })),
-  model: z.string()
+  model: z.string(),
+  provider: z.string()
 })
 
 type RequestSchema = z.infer<typeof requestSchema>
+type ChatParams = Omit<RequestSchema, 'provider'>
