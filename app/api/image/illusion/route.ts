efec1853357ebfac71f4f6b808c8c9a824fc9204
Dean Kerr <dean.kerr@gmail.com>
2023-10-02 22:19:09 +1100

    add provider/fal openapi-fetch

--- app/api/image/illusion/route.ts
+++ app/api/image/illusion/route.ts
@@ -1,27 +1,16 @@
-import { env, logger } from '@/lib/utils'
+import { illusion } from '@/lib/provider/fal/'
+import { logger } from '@/lib/utils'
 import { NextRequest, NextResponse } from 'next/server'
 import { z } from 'zod'
 
-const log = logger.child({ api: 'image/illusion' }, { msgPrefix: '[API/ILLUSION] ' })
+const log = logger.child({}, { msgPrefix: '[API/ILLUSION] ' })
 
 export async function POST(request: NextRequest) {
   log.info('POST image/illusion')
   const params = requestSchema.parse(await request.json())
-  log.info(params, 'parameters')
+  log.info('%o', params)
 
-  const url = 'https://54285744-illusion-diffusion.gateway.alpha.fal.ai/'
-  const options = {
-    method: 'POST',
-    headers: {
-      Authorization: `Key ${env('FALAI_API_KEY')}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(params),
-  }
-
-  const response = await fetch(url, options)
-  const data = await response.json()
-  const result = responseSchema.parse(data)
+  const result = await illusion(params)
   log.info(result, 'result')
 
   return NextResponse.json({
@@ -33,10 +22,4 @@ const requestSchema = z.object({
   image_url: z.string().url(),
   prompt: z.string(),
   negative_prompt: z.string().optional(),
-})
-
-const responseSchema = z.object({
-  image: z.object({
-    url: z.string().url(),
-  }),
 })
