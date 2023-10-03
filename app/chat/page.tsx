c4e1d6cf6d037197590d2293aec640e2f1527c95
Dean Kerr <dean.kerr@gmail.com>
2023-10-03 18:49:44 +1100

    chatUI: add fixed-to-bottom input panel

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,3 +1,5 @@
+import { PaperAirplaneIcon } from '@heroicons/react/24/outline'
+
 type ChatMessageItem = {
   role: 'system' | 'user' | 'assistant'
   name?: string
@@ -6,40 +8,46 @@ type ChatMessageItem = {
 
 export default function Chat() {
   return (
-    <div className="min-h-full min-w-full bg-base-300">
-      <div className="flex flex-col justify-end min-h-screen max-w-md mx-auto bg-base-100">
+    <div className="min-h-screen">
+      <div className="mx-auto flex min-h-screen max-w-md flex-col justify-end border-x border-base-200 bg-base-100 pb-24">
         <div className="px-2 pb-2">
-          {_sampleMessages.map((m, i) => (
-            <MessageItem message={m} key={i} />
-          ))}
+          {_sampleMessages.map((msg, i) =>
+            msg.role === 'system' ? (
+              <SystemBubble content={msg.content} key={i} />
+            ) : (
+              <MessageBubble message={msg} key={i} />
+            ),
+          )}
         </div>
-        <div className="flex justify-center bg-base-200 py-2">
-          <InputBox />
-        </div>
+        <InputPanel />
       </div>
     </div>
   )
 }
 
-const bubbleColor: Record<string, string> = {
-  system: 'chat-bubble-accent',
-  user: 'chat-bubble-primary',
-  assistant: 'chat-bubble-secondary',
+function InputPanel() {
+  return (
+    <div className="fixed bottom-0 flex w-full max-w-md justify-center bg-base-200 px-4 py-2">
+      <InputBox />
+    </div>
+  )
 }
 
-function MessageItem({ message }: { message: ChatMessageItem }) {
-  const color = bubbleColor[message.role]
-  const alignment = message.role === 'assistant' ? 'chat-end' : 'chat-start'
-  const name = message.name ?? message.role
+function SystemBubble(props: { content: string }) {
+  const { content } = props
+  return (
+    <div className="mx-auto max-w-sm">
+      <div className="text-sm">System</div>
+      <div className="rounded-lg bg-accent px-4 py-1 text-center">{content}</div>
+    </div>
+  )
+}
 
-  if (message.role === 'system') {
-    return (
-      <div className="max-w-sm mx-auto">
-        <div className="text-sm">system</div>
-        <div className="bg-accent px-4 py-1 text-center rounded-lg">{message.content}</div>
-      </div>
-    )
-  }
+function MessageBubble({ message }: { message: ChatMessageItem }) {
+  const { role } = message
+  const color = role === 'user' ? 'chat-bubble-primary' : 'chat-bubble-secondary'
+  const alignment = role === 'user' ? 'chat-start' : 'chat-end'
+  const name = message.name ?? (role === 'user' ? 'User' : 'Assistant')
 
   return (
     <div className={`chat ${alignment}`}>
@@ -51,10 +59,18 @@ function MessageItem({ message }: { message: ChatMessageItem }) {
 
 function InputBox() {
   return (
-    <textarea
-      placeholder="Enter your message..."
-      className="textarea textarea-bordered textarea-sm w-full max-w-xs"
-    ></textarea>
+    <div className="flex w-full justify-center gap-2 align-middle">
+      <textarea
+        placeholder="Enter your message..."
+        className="textarea textarea-bordered textarea-sm flex-auto"
+      ></textarea>
+
+      <div className="flex flex-col justify-center">
+        <button className="btn btn-circle btn-accent">
+          <PaperAirplaneIcon className="w-8" />
+        </button>
+      </div>
+    </div>
   )
 }
 
@@ -63,11 +79,43 @@ const _sampleMessages: ChatMessageItem[] = [
   {
     role: 'user',
     name: 'Katelynn',
-    content: 'Elit quis ut nisi aliquip cupidatat occaecat aute laboris velit laborum.',
+    content: 'In elit laborum sunt sint consequat incididunt eiusmod occaecat.',
   },
   {
     role: 'assistant',
     content:
-      'Labore duis anim commodo minim tempor mollit et dolore enim exercitation minim. Adipisicing in do duis mollit nisi duis amet ipsum quis. Minim est amet aliquip nisi consectetur laboris quis nulla exercitation sit excepteur eu. Et tempor ex fugiat tempor cillum. Nostrud officia esse veniam mollit irure.',
+      'Et amet ullamco exercitation ad nulla esse adipisicing do cupidatat cillum non sit excepteur reprehenderit cillum. Commodo in laborum consequat quis deserunt esse. Eiusmod ex magna eu culpa id dolore culpa laboris tempor. Ea laboris excepteur sint dolor elit nisi anim exercitation ipsum sunt cupidatat consectetur labore aliqua nostrud. Ullamco dolore laboris id.',
+  },
+  {
+    role: 'user',
+    name: 'Katelynn',
+    content: 'Ut pariatur fugiat irure anim mollit ad amet.',
+  },
+  {
+    role: 'assistant',
+    content:
+      'Amet ad Lorem cillum. Minim aliqua culpa exercitation aute ipsum sunt incididunt culpa quis culpa duis eu consectetur duis esse. Laboris consequat occaecat deserunt consequat cupidatat nisi. Nostrud duis officia velit dolore reprehenderit eiusmod id non elit aliqua adipisicing elit. Ad duis mollit enim aliqua pariatur non magna ad amet id ad dolore nulla incididunt dolore. Sunt ex consectetur aliqua dolor aliquip. Elit et quis pariatur cillum dolore culpa consequat nisi duis ad amet sint duis commodo proident.',
+  },
+  {
+    role: 'user',
+    name: 'Katelynn',
+    content: 'Eiusmod non sit cillum dolore nulla.',
+  },
+  {
+    role: 'assistant',
+    content:
+      'Officia sint in adipisicing ex exercitation. Quis voluptate cupidatat consequat Lorem aute exercitation anim mollit amet labore cupidatat qui Lorem exercitation. Eu tempor non dolor nulla esse culpa. Aliqua in non mollit tempor ea. Dolore ex aliqua esse proident amet ullamco commodo proident amet esse non pariatur. Sunt incididunt non veniam ea enim veniam voluptate.',
+  },
+  {
+    role: 'user',
+    name: 'Katelynn',
+    content: 'Minim ut incididunt eu proident.',
+  },
+  {
+    role: 'assistant',
+    content:
+      'Elit dolore aliqua in voluptate anim quis pariatur amet elit laboris ut amet et nostrud. Amet laboris cillum esse sunt commodo irure. Dolore laborum ad velit cillum sunt. Do elit occaecat est dolore proident in qui minim exercitation excepteur et nisi proident pariatur cupidatat. Nulla consequat ex pariatur exercitation officia eu est. Aliquip cupidatat reprehenderit aliquip ad commodo voluptate id et cupidatat aliquip eu sint non. Qui excepteur proident voluptate id ut consequat fugiat.',
   },
 ]
+
+const _sampleMessagesFew = _sampleMessages.slice(0, 3)
