fd467eeffa09871afd222f65cd964af7c173ad43
Dean Kerr <dean.kerr@gmail.com>
2023-10-06 13:13:52 +1100

    add custom MessageBubbles

--- app/chat/components/MessageBubble.tsx
+++ app/chat/components/MessageBubble.tsx
@@ -1,12 +1,12 @@
 import { Markdown } from '@/components/Markdown'
 import type { ChatMessage } from '../Chat'
 
-export function SystemBubble(props: { content: string }) {
+export function MessageBubbleSystem(props: { content: string }) {
   const { content } = props
   return (
-    <div className="mx-6 w-auto">
+    <div className="flex flex-col items-center py-1">
       <div className="text-sm text-transparent">System</div>
-      <div className="rounded-xl border border-primary bg-neutral px-4 py-1 text-center text-neutral-content shadow-lg">
+      <div className="w-fit max-w-[90%] rounded-xl border border-accent bg-neutral px-4 py-1 text-center text-neutral-content shadow-lg">
         {content}
       </div>
     </div>
@@ -16,6 +16,24 @@ export function SystemBubble(props: { content: string }) {
 export function MessageBubble({ message }: { message: ChatMessage }) {
   const isUser = message.role === 'user'
 
+  return (
+    <div className={`flex flex-col ${isUser ? 'items-start' : 'items-end'} py-1`}>
+      <div className="w-fit px-1 text-sm">{message.name ?? (isUser ? 'User' : 'Assistant')}</div>
+      <div
+        className={`w-fit max-w-[90%] rounded-xl border ${
+          isUser ? 'border-primary' : 'border-secondary'
+        } bg-neutral px-4 py-2 text-neutral-content`}
+      >
+        <div className="max-w prose prose-neutral">
+          <Markdown>{message.content}</Markdown>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function ChatBubble({ message }: { message: ChatMessage }) {
+  const isUser = message.role === 'user'
   return (
     <div className={`chat ${isUser ? 'chat-start' : 'chat-end'}`}>
       <div className="chat-header text-transparent">
--- app/chat/components/MessagePanel.tsx
+++ app/chat/components/MessagePanel.tsx
@@ -1,15 +1,15 @@
 import type { ChatMessage } from '../Chat'
-import { MessageBubble, SystemBubble } from './MessageBubble'
+import { ChatBubble, MessageBubble, MessageBubbleSystem } from './MessageBubble'
 
 export function MessagePanel({ messages }: { messages: ChatMessage[] }) {
   return (
     <div
-      className="flex-1 overflow-y-auto overflow-x-clip px-3 pb-3"
+      className="flex-1 overflow-y-auto overflow-x-clip px-4 pb-3"
       onClick={() => console.log('[MessagePanel]', messages)}
     >
       {messages.map((msg, i) =>
         msg.role === 'system' ? (
-          <SystemBubble content={msg.content} key={i} />
+          <MessageBubbleSystem content={msg.content} key={i} />
         ) : (
           <MessageBubble message={msg} key={i} />
         ),
