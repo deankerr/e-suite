c0c04a42d4f87ad5b41f7c571058f4b5f644914d
Dean Kerr <dean.kerr@gmail.com>
2023-10-08 19:09:18 +1100

    add flagged strings to error response message

--- lib/providers/openai/index.ts
+++ lib/providers/openai/index.ts
@@ -1,6 +1,8 @@
-import { createErrorResponse, raise } from '@/lib/utils'
+import { createErrorResponse, logger, raise } from '@/lib/utils'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import OpenAI from 'openai'
+
+const log = logger.child({}, { msgPrefix: '[provider/openai] ' })
 
 const api = new OpenAI()
 
@@ -11,7 +13,7 @@ export const openai = {
 }
 
 async function chat(input: OpenAI.Chat.ChatCompletionCreateParams) {
-  console.log('openai.chat')
+  log.info('chat')
   if (input.stream) {
     const response = await api.chat.completions.create(input)
     const stream = OpenAIStream(response)
@@ -24,23 +26,23 @@ async function chat(input: OpenAI.Chat.ChatCompletionCreateParams) {
 }
 
 async function chatModerated(input: OpenAI.Chat.ChatCompletionCreateParams) {
-  console.log('openai.chatModerated')
+  log.info('chatModerated')
   const messages = input.messages.map((m) => `${m.content}`)
   const response = await api.moderations.create({ input: messages })
   const flagged = input.messages.filter((_, i) => response.results[i]?.flagged)
 
   if (flagged.length === 0) {
-    console.log('allow chat')
+    log.info('allow chat')
     return chat(input)
   } else {
-    console.log(flagged)
-    console.log('reject chat')
-    return Response.json(createErrorResponse({ message: 'Moderation failed' }))
+    log.warn(flagged, 'reject chat')
+    const message = `OpenAI Moderation rejected: ${flagged.map((m) => `"${m.content}"`).join(', ')}`
+    return Response.json(createErrorResponse({ status: 403, message }))
   }
 }
 
 async function image(input: OpenAI.ImageGenerateParams) {
-  console.log('openai.image')
+  log.info('image')
   try {
     const response = await api.images.generate(input)
     const item = { url: response.data[0]?.url ?? raise('response missing expected url') }
