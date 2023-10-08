0861eecc1af5c6007f028ffce5f4ece38aa54ade
Dean Kerr <dean.kerr@gmail.com>
2023-10-08 15:36:00 +1100

    add Chat demo route, remove homepage for now

---
+++ app/turby/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Chat } from '@/components/Chat/Chat'
+import { useIsClient } from '@uidotdev/usehooks'
+
+export default function TurbyChatPage() {
+  // stop Chat from rendering on server due to localStorage (?)
+  const isClient = useIsClient()
+
+  return isClient ? (
+    <Chat
+      model="gpt-3.5-turbo"
+      provider="openai"
+      prompt="You are a helpful AI assistant named Turby. Use Markdown in your responses."
+      title="turbys tavern"
+      names={{ user: 'Greg', assistant: 'Turby' }}
+    />
+  ) : null
+}
