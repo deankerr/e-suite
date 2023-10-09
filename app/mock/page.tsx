a1925f048bac2bfbc4a7782816976fd1c00d2b7c
Dean Kerr <dean.kerr@gmail.com>
2023-10-09 15:33:30 +1100

    add orange theme, more demo components

--- app/mock/page.tsx
+++ app/mock/page.tsx
@@ -1,8 +1,11 @@
 import { CardDemo } from '@/components/card-demo'
+import { ContextMenuDemo } from '@/components/context-menu-demo'
 import { NavigationMenuDemo } from '@/components/main-nav'
 import { SidebarDemo } from '@/components/sidebar-demo'
 import { TableDemo } from '@/components/table-demo'
+import { TabsDemo } from '@/components/tabs-demo'
 import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
 import { ThemeToggle } from '@/components/ui/theme-toggle'
 
 export default function MockPage() {
@@ -13,9 +16,7 @@ export default function MockPage() {
         id="ui-header"
         className="flex h-16 w-full flex-row items-center justify-between border-2 border-border bg-background px-8 text-foreground"
       >
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          shadcn/ui
-        </h1>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight">shadcn/ui</h1>
         <NavigationMenuDemo />
         <ThemeToggle />
       </div>
@@ -27,9 +28,16 @@ export default function MockPage() {
 
         {/* Main Content */}
         <main className="border-2 border-border bg-background p-4 py-6">
-          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-            Main Content Area <Button>buton</Button>
-          </h3>
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Main Content Area</h3>
+
+          <div className="mt-6 flex justify-center gap-2">
+            <Button>Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="destructive">Destructive</Button>
+            <Button variant="outline">Outline</Button>
+            <Button variant="ghost">Ghost</Button>
+            <Button variant="link">Link</Button>
+          </div>
 
           <p className="leading-7 [&:not(:first-child)]:mt-6">
             Ea voluptate proident fugiat fugiat sint voluptate elit. Ad qui nulla ut sint et sit
@@ -37,7 +45,7 @@ export default function MockPage() {
             nostrud irure consectetur. Adipisicing veniam ullamco irure in enim. Enim laborum
             reprehenderit dolore nostrud excepteur veniam ad voluptate sunt enim.
           </p>
-          <CardDemo className="float-right" />
+
           <p className="leading-7 [&:not(:first-child)]:mt-6">
             Qui laboris consequat est eu consequat ex tempor nostrud pariatur aliquip cupidatat ad
             eu. Excepteur nostrud laboris ea irure sint exercitation ad mollit ea deserunt. Laboris
@@ -45,18 +53,31 @@ export default function MockPage() {
             Consequat sit ex magna mollit consequat. Esse ullamco duis aute laboris est laborum
             occaecat consectetur. Sunt sunt fugiat veniam magna dolore dolor esse in Lorem occaecat.
           </p>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            Anim commodo dolor ullamco tempor ut laborum. Enim ullamco elit incididunt deserunt
-            mollit minim pariatur. Aliquip pariatur duis laboris sint est do anim exercitation ea
-            amet est reprehenderit aute. Lorem incididunt amet eu non anim id irure nostrud ad
-            officia elit veniam ex elit. In do aute reprehenderit exercitation quis elit ea
-            incididunt occaecat exercitation proident.
-          </p>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            Ut anim labore esse nisi. Veniam aliqua officia ipsum elit reprehenderit. Velit
-            incididunt qui pariatur irure. Mollit amet consequat sit id consectetur veniam qui.
+
+          <p className="prose mt-6">
+            Ea voluptate proident fugiat fugiat sint voluptate elit. Ad qui nulla ut sint et sit
+            ullamco qui. Veniam consequat sint deserunt qui adipisicing reprehenderit occaecat esse
+            nostrud irure consectetur. Adipisicing veniam ullamco irure in enim. Enim laborum
+            reprehenderit dolore nostrud excepteur veniam ad voluptate sunt enim.
           </p>
 
+          <p className="prose mt-6">
+            Qui laboris consequat est eu consequat ex tempor nostrud pariatur aliquip cupidatat ad
+            eu. Excepteur nostrud laboris ea irure sint exercitation ad mollit ea deserunt. Laboris
+            culpa aliqua exercitation officia. Dolor cillum elit cillum exercitation voluptate.
+            Consequat sit ex magna mollit consequat. Esse ullamco duis aute laboris est laborum
+            occaecat consectetur. Sunt sunt fugiat veniam magna dolore dolor esse in Lorem occaecat.
+          </p>
+
+          <div className="flex flex-col items-center">
+            <Separator className="my-4" />
+            <TabsDemo />
+            <Separator className="my-4" />
+            <ContextMenuDemo />
+            <Separator className="my-4" />
+            <CardDemo />
+            <Separator className="my-4" />
+          </div>
           <TableDemo />
         </main>
 
