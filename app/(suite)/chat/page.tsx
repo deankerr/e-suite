2ae0884edc3a36dc43cb59bf394da7beedc84af8
Dean Kerr <dean.kerr@gmail.com>
2023-10-23 12:58:46 +1100

    test layout with ChatPanels

--- app/(suite)/chat/page.tsx
+++ app/(suite)/chat/page.tsx
@@ -1,5 +1,26 @@
+import { ChatPanel } from '@/components/chat/chat-panel'
+import { initialChatsConfig } from '@/components/chat/config'
+import { getChatModels } from '@/lib/api'
+
 type Props = {}
 
 export default function ChatPage(props: Props) {
-  return <div className="h-full w-full">ChatPage</div>
+  const modelsAvailable = getChatModels()
+
+  return (
+    <div className="flex h-full overflow-y-hidden overflow-x-scroll">
+      {initialChatsConfig.map((chat) => (
+        <ChatPanel
+          key={chat.id}
+          session={chat}
+          updateSession={dummyUpdateSession}
+          modelsAvailable={modelsAvailable}
+        />
+      ))}
+    </div>
+  )
+}
+
+async function dummyUpdateSession() {
+  'use server'
 }
