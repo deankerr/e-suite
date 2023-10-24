ea8bedb3e76d9444fa3179f561be03c280c2f814
Dean Kerr <dean.kerr@gmail.com>
2023-10-24 15:06:43 +1100

    add client side tab switching demo

--- app/(suite)/chat/page.tsx
+++ app/(suite)/chat/page.tsx
@@ -1,30 +1,50 @@
+'use client'
+
 import { ChatPanelTab } from '@/components/chat/chat-panel-tab'
 import { initialChatsConfig } from '@/components/chat/config'
 import { getChatModels } from '@/lib/api'
+import { cn } from '@/lib/utils'
 import { DotFilledIcon } from '@radix-ui/react-icons'
+import { useState } from 'react'
 
 type Props = {}
 
 export default function ChatPage(props: Props) {
   const modelsAvailable = getChatModels()
 
-  const tab = initialChatsConfig[1]!
+  const [currentTab, setCurrentTab] = useState(initialChatsConfig[1]!)
 
   return (
     <>
-      <div className="h-10 flex-none bg-muted">
-        <div className="flex h-full w-40 items-center justify-between border-t-2 border-primary bg-background px-3 text-sm font-medium">
-          <div className="px-1"></div>
-          {tab.panel.title}
-          <DotFilledIcon />
-        </div>
+      <div className="flex h-10 flex-none bg-muted">
+        {initialChatsConfig.map((c) => (
+          <Tab
+            key={c.id}
+            title={c.panel.title}
+            active={c.id === currentTab.id}
+            onClick={() => setCurrentTab(c)}
+          />
+        ))}
       </div>
 
-      <ChatPanelTab session={tab} modelsAvailable={modelsAvailable} />
+      <ChatPanelTab session={currentTab} modelsAvailable={modelsAvailable} />
     </>
   )
 }
 
-async function dummyUpdateSession() {
-  'use server'
+function Tab(props: { title: string; active: boolean } & React.ComponentProps<'div'>) {
+  const { title, active, ...rest } = props
+  return (
+    <div
+      className={cn(
+        'flex h-full w-40 items-center justify-between border-primary px-3 text-sm font-medium',
+        active ? 'border-t-2 bg-background' : 'bg-muted',
+      )}
+      {...rest}
+    >
+      <div className="px-1"></div>
+      {title}
+      <DotFilledIcon />
+    </div>
+  )
 }
