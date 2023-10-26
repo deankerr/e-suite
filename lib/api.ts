8915a5f99b13f0844aeff668e0f39f7ffd97e97e
Dean Kerr <dean.kerr@gmail.com>
2023-10-27 06:55:40 +1100

    remove unused api functions

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-import { env, raise } from './utils'
+import { env } from './utils'
 
 export function createErrorResponse(message: string, status = 400) {
   return new Response(message, { status, statusText: message })
@@ -26,56 +26,6 @@ export function getGuestAuthKeys() {
     env('GUEST_AUTH_KEY_6'),
   ]
 }
-//# deprecate
-export function getChatModels() {
-  return models
-}
-//# deprecate
-export type ChatModelOption = {
-  id: string
-  provider: string
-  model: string
-  label: string
-}
-//# deprecate
-const models: ChatModelOption[] = [
-  {
-    id: 'openai::gpt-3.5-turbo',
-    provider: 'openai',
-    model: 'gpt-3.5-turbo',
-    label: 'OpenAI: GPT-3.5 Turbo',
-  },
-  {
-    id: 'openai::gpt-4',
-    provider: 'openai',
-    model: 'gpt-4',
-    label: 'OpenAI: GPT-4',
-  },
-  {
-    id: 'openrouter::meta-llama/llama-2-70b-chat',
-    provider: 'openrouter',
-    model: 'meta-llama/llama-2-70b-chat',
-    label: 'Meta: Llama v2 70B Chat',
-  },
-  {
-    id: 'openrouter::jondurbin/airoboros-l2-70b',
-    provider: 'openrouter',
-    model: 'jondurbin/airoboros-l2-70b',
-    label: 'Airoboros L2 70B',
-  },
-  {
-    id: 'openrouter::migtissera/synthia-70b',
-    provider: 'openrouter',
-    model: 'migtissera/synthia-70b',
-    label: 'Synthia 70B',
-  },
-  {
-    id: 'openrouter::xwin-lm/xwin-lm-70b',
-    provider: 'openrouter',
-    model: 'xwin-lm/xwin-lm-70b',
-    label: 'Xwin 70B',
-  },
-]
 
 export function getAvailableChatModels() {
   return chatModels
--- lib/utils.ts
+++ lib/utils.ts
@@ -14,18 +14,6 @@ export const logger = pino({
   redact: ['base64', 'image_url'],
 })
 
-const truncateLength = 40
-function truncateLogString(obj: Record<string, unknown>, ...keys: string[]) {
-  for (const key of keys) {
-    if (key in obj && typeof obj[key] === 'string') {
-      const value = obj[key] as string
-      if (value.length > truncateLength) {
-        obj[key] = value.slice(0, truncateLength) + ' ... [TRUNCATED]'
-      }
-    }
-  }
-}
-
 export function raise(message: string): never {
   throw new Error(message)
 }
