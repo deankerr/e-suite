9d6979e2b0a9dd16a16570fc32942379f2b159f6
Dean Kerr <dean.kerr@gmail.com>
2023-11-29 21:17:25 +1100

    move staging branch url to env vars from script

--- next.config.js
+++ next.config.js
@@ -4,13 +4,17 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+// use standard git staging url for staging branch
+const url =
+  process.env.VERCEL_GIT_COMMIT_REF === 'staging'
+    ? process.env.VERCEL_BRANCH_URL
+    : process.env.VERCEL_URL
+
 const nextConfig = {
   env: {
-    KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
-    KINDE_POST_LOGOUT_REDIRECT_URL:
-      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
-    KINDE_POST_LOGIN_REDIRECT_URL:
-      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/`,
+    KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${url}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL: process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${url}`,
+    KINDE_POST_LOGIN_REDIRECT_URL: process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${url}`,
   },
 }
 
