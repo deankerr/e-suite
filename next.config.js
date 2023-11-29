f5e686f327f4be7a58fe7c23cff09b951ac9b740
Dean Kerr <dean.kerr@gmail.com>
2023-11-29 19:05:57 +1100

    add kinde auth preview scripts

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-module.exports = withBundleAnalyzer({})
+
+const nextConfig = {
+  env: {
+    KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/`,
+  },
+}
+
+module.exports = withBundleAnalyzer(nextConfig)
