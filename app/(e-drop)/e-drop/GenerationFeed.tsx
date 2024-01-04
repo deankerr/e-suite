5a4101a68e30c0a5e996561f39bb0aef69e9a137
Dean Kerr <dean.kerr@gmail.com>
2024-01-04 17:36:49 +1100

    add missing key prop to sample feed

--- app/(e-drop)/e-drop/GenerationFeed.tsx
+++ app/(e-drop)/e-drop/GenerationFeed.tsx
@@ -12,13 +12,13 @@ export const GenerationFeed = ({ props }: GenerationFeedProps) => {
   return (
     <ScrollArea className="dark:content-area-inset-shadow transition-all duration-300 md:mr-80">
       <div className="flex flex-col items-center gap-8 px-4 py-6">
-        {generationSampleUrls.flatMap((url) => (
-          <Card className="w-full max-w-3xl">
+        {generationSampleUrls.map((urls, i) => (
+          <Card key={i} className="w-full max-w-3xl">
             <div className="grid grid-cols-2 gap-4 p-6 md:grid-cols-3">
-              {url.map((urlValue) => (
+              {urls.map((url) => (
                 <Image
-                  key={urlValue}
-                  src={'/generations-sample/' + urlValue}
+                  key={url}
+                  src={'/generations-sample/' + url}
                   alt="generation"
                   width={512}
                   height={512}
