9bedfd4cc258c0caf457247d76156654c05e134e
Dean Kerr <dean.kerr@gmail.com>
2024-01-25 06:06:34 +0000

    add floating generation controls ui

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { SignInButton, UserButton } from '@clerk/nextjs'
-import { Card } from '@radix-ui/themes'
+import { Id } from '@/convex/_generated/dataModel'
+import { Button, Card, TextArea } from '@radix-ui/themes'
 import logo from '/assets/icons/logo-sunset.svg'
 import { useConvexAuth } from 'convex/react'
 import Image from 'next/image'
-import Link from 'next/link'
+import { ImageModelCard } from './ImageModelCard'
+import { DimensionsToggleGroup } from './ui/DimensionsToggleGroup'
 import { useRegisterUser } from './useRegisterUser'
 
 type NavbarProps = {
@@ -17,15 +18,37 @@ export const Navbar = ({ props }: NavbarProps) => {
   const userId = useRegisterUser()
 
   return (
-    <Card className="z-30 place-self-start opacity-60 hover:opacity-100" size="1">
-      <nav className="flex items-center justify-between gap-1">
-        <Link href="/" className="flex items-center gap-2">
-          <Image src={logo} alt="e/drop logo" className="-my-0.5 -mr-0.5 size-9" priority />
-        </Link>
+    <Card
+      className="z-30 self-start justify-self-center bg-panel-solid opacity-100 hover:opacity-100"
+      size="1"
+    >
+      <div className="flex gap-2.5">
+        <div className="grid h-rx-9 place-content-center">
+          <Image src={logo} alt="e/suite sun logo" className="size-9" />
+        </div>
 
-        {!isLoading && !isAuthenticated && <SignInButton mode="modal" />}
-        <UserButton afterSignOutUrl="/" />
-      </nav>
+        <div className="grid max-w-2xl gap-2 md:grid-cols-2">
+          <TextArea placeholder="what do you want to see?" className="" />
+          <TextArea placeholder="what do you not want to see?" className="" />
+
+          <ImageModelCard
+            id={'k574mz0xgj33gmb0ms469jwrpn6hf2zz' as Id<'imageModels'>}
+            className=""
+          />
+          <div className="flex flex-col justify-between gap-2">
+            <DimensionsToggleGroup />
+            {/* <Separator size="4" /> */}
+            <Button variant="surface">Generate</Button>
+          </div>
+        </div>
+      </div>
     </Card>
   )
+}
+
+{
+  /* {!isLoading && !isAuthenticated && <SignInButton mode="modal" />} */
+}
+{
+  /* <UserButton afterSignOutUrl="/" /> */
 }
---
+++ app/components/ui/DimensionsToggleGroup.tsx
@@ -0,0 +1,46 @@
+import { TextAlignCenterIcon, TextAlignLeftIcon, TextAlignRightIcon } from '@radix-ui/react-icons'
+import * as ToggleGroup from '@radix-ui/react-toggle-group'
+import { Button } from '@radix-ui/themes'
+import { RectangleHorizontalIcon, RectangleVerticalIcon, SquareIcon } from 'lucide-react'
+import React from 'react'
+
+const toggleGroupItemClasses: TailwindClass =
+  'data-[state=on]:bg-accent-3 rounded-none first:rounded-l last:rounded-r focus:z-10 focus:shadow-[0_0_0_2px] focus:outline-none'
+
+export const DimensionsToggleGroup = () => (
+  <ToggleGroup.Root
+    className="grid grid-cols-3 gap-1"
+    type="single"
+    defaultValue="center"
+    aria-label="Generate Image Dimensions"
+  >
+    <ToggleGroup.Item
+      className={toggleGroupItemClasses}
+      value="portrait"
+      aria-label="Portrait"
+      asChild
+    >
+      <Button variant="outline" className="h-fit cursor-pointer flex-col py-2" size="1">
+        <RectangleVerticalIcon />
+        Portrait
+      </Button>
+    </ToggleGroup.Item>
+    <ToggleGroup.Item className={toggleGroupItemClasses} value="square" aria-label="Square" asChild>
+      <Button variant="outline" className="h-fit cursor-pointer flex-col py-2" size="1">
+        <SquareIcon />
+        Square
+      </Button>
+    </ToggleGroup.Item>
+    <ToggleGroup.Item
+      className={toggleGroupItemClasses}
+      value="landscape"
+      aria-label="Landscape"
+      asChild
+    >
+      <Button variant="outline" className="h-fit cursor-pointer flex-col py-2" size="1">
+        <RectangleHorizontalIcon />
+        Landscape
+      </Button>
+    </ToggleGroup.Item>
+  </ToggleGroup.Root>
+)
