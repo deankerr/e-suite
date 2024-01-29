533b863dadf9cfedb7ae9dd2b23ccc2133502985
Dean Kerr <dean.kerr@gmail.com>
2024-01-29 23:14:08 +0000

    add dev only next config for jotai plugins, remove withPlaiceholder

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,42 @@
-/** @type {import('next').NextConfig} */
-import withPlaiceholder from '@plaiceholder/next'
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js'
 
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'artful-husky-972.convex.cloud',
-        pathname: '/api/storage/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'artful-husky-972.convex.site',
-        pathname: '/image',
-      },
-    ],
-  },
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-    swcPlugins: [['@swc-jotai/react-refresh', {}]],
-  },
+/** @type {import('next').NextConfig} */
+
+const images = {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'artful-husky-972.convex.cloud',
+      pathname: '/api/storage/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'artful-husky-972.convex.site',
+      pathname: '/image',
+    },
+  ],
 }
 
-export default withPlaiceholder(nextConfig)
+const config = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      images,
+      experimental: {
+        missingSuspenseWithCSRBailout: false,
+        swcPlugins: [
+          ['@swc-jotai/react-refresh', {}],
+          ['@swc-jotai/debug-label', {}],
+        ],
+      },
+    }
+  }
+
+  return {
+    images,
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
+  }
+}
+
+export default config
