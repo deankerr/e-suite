32d30d35de104238e4866eb2684760368e9ee065
Dean Kerr <dean.kerr@gmail.com>
2024-02-11 20:56:27 +1100

    add v2 generations/images schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,10 +1,54 @@
 import { defineEnt, defineEntSchema, getEntDefinitions } from 'convex-ents'
 import { v } from 'convex/values'
+import { nsfwRatings } from './constants'
 import { imagesEnt } from './files/images'
 import { generationsEnt } from './generations'
 import { imageModelEnt } from './imageModels'
 import { messagesFields } from './threads/messages'
 import { vEnum } from './util'
+
+const permissions = v.object({
+  private: v.boolean(), // only visible by owner/author
+  allowOnPublicFeeds: v.boolean(),
+  allowOtherUsersToEdit: v.boolean(),
+})
+
+const generationParameters = {
+  imageModelId: v.string(), //TODO v.id
+  prompt: v.string(),
+  negativePrompt: v.optional(v.string()),
+  seed: v.optional(v.number()),
+  scheduler: v.optional(v.string()),
+  steps: v.optional(v.number()),
+  guidance: v.optional(v.number()),
+  lcm: v.optional(v.boolean()),
+}
+
+const v2imagesFields = {
+  storageId: v.optional(v.id('_storage')),
+  url: v.optional(v.string()),
+
+  sourceUrl: v.optional(v.string()),
+  nsfw: vEnum(nsfwRatings),
+
+  width: v.number(),
+  height: v.number(),
+  blurDataURL: v.string(),
+  color: v.string(),
+  metadata: v.any(),
+
+  parameters: v.optional(v.object(generationParameters)),
+  permissions,
+}
+const v2images = defineEnt(v2imagesFields).deletion('soft').index('sourceUrl', ['sourceUrl'])
+
+const v2generationsFields = {
+  permissions,
+  images: v.array(v.object(v2imagesFields)),
+}
+const v2generations = defineEnt(v2generationsFields)
+  .deletion('soft')
+  .edge('user', { field: 'authorId' })
 
 const jobStatusNames = vEnum([
   'pending',
@@ -21,11 +65,8 @@ export const jobEventFields = {
   message: v.optional(v.string()),
   data: v.optional(v.any()),
 }
-
 export const jobTypes = vEnum(['llm', 'throw'])
-
 export const jobRefs = v.union(v.id('messages'), v.id('generations'))
-
 export const jobFields = {
   type: jobTypes,
   ref: jobRefs,
@@ -51,22 +92,33 @@ const schema = defineEntSchema(
     })
       .edge('user', { field: 'ownerId' })
       .deletion('soft'),
+
     clerkWebhookEvents: defineEnt({
       body: v.string(),
       id: v.string(),
       type: v.string(),
     }).deletion('soft'),
     generations: generationsEnt.deletion('soft'),
+
+    v2generations,
+    v2images,
+
     images: imagesEnt.deletion('soft'),
+
     imageModels: imageModelEnt.deletion('soft'),
+
     jobs: defineEnt(jobFields).deletion('soft'),
+
     messages: defineEnt(messagesFields).edge('thread', { field: 'threadId' }).deletion('soft'),
+
     threads: defineEnt({ name: v.string(), systemPrompt: v.string() })
       .edge('user', { field: 'ownerId' })
       .edges('messages', { ref: 'threadId', deletion: 'soft' })
       .deletion('soft'),
+
     users: defineEnt(usersFields)
       .deletion('soft')
+      .edges('v2generations', { ref: 'authorId' })
       .edges('threads', { ref: 'ownerId' })
       .edge('apiKey', { optional: true, ref: 'ownerId' })
       .field('tokenIdentifier', v.string(), { index: true }),
