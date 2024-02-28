8c9a95f05f18cb4420686a6fd39ec0715b18afbe
Dean Kerr <dean.kerr@gmail.com>
2024-02-28 16:55:07 +1100

    refactor title generation request

--- convex/threads/lib/inference.ts
+++ convex/threads/lib/inference.ts
@@ -37,7 +37,7 @@ export const generateThreadTitle = internalAction({
   args: {
     threadId: v.id('threads'),
   },
-  handler: async (ctx, { threadId }) => {
+  handler: async (ctx, { threadId }): Promise<void> => {
     const thread = await ctx.runQuery(internal.threads.threads.internalGet, {
       id: threadId,
     })
@@ -47,16 +47,31 @@ export const generateThreadTitle = internalAction({
     const conversation = thread.messages
       .reverse()
       .reduce<string>((acc, curr) => acc.concat(`${curr.role}: ${curr.content}\n`), '')
+      .trim()
 
-    console.log({ prompt: conversation })
-    const title = await sendChat({ prompt: conversation })
-    console.log('title:', title)
+    const prompt = `You are a summarization system that can provide summaries of a conversation. In clear and concise language, provide a short summary of the following conversation.
+    Afterward, create a 3 to 6 word title for the conversation, strictly adhering to the word limit and avoiding use of the words 'title' or 'user'. Enclose the title in <<<>>>.
+    
+    # Conversation
+    ${conversation}
+
+    # Summary`
+
+    console.log({ prompt })
+    const response = await ai().completions.create({
+      prompt,
+      model: 'mistralai/Mistral-7B-Instruct-v0.2',
+    })
+
+    const result = response.choices[0]?.text
+    console.log(result)
+
+    const title = result?.match(/<<<(.*)>>>/)?.[1]
+    assert(title, 'Invalid title')
 
     await ctx.runMutation(internal.threads.threads.internalUpdate, {
       id: threadId,
       fields: { title: title.trim(), permissions: thread.permissions },
     })
-
-    return 0
   },
 })
