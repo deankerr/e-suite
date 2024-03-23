fd7412044c397b4a1f3c10c812890413305d0834
Dean Kerr <dean.kerr@gmail.com>
2024-03-23 20:11:11 +1100

    remove spacer from Sidebar, handle with content margin

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -15,28 +15,22 @@ export const Sidebar = forwardRef<HTMLDivElement, SidebarProps>(function Sidebar
     'w-full md:z-10 pointer-events-auto z-40 bg-overlay md:bg-transparent md:pointer-events-none md:w-80'
 
   return (
-    // container / spacer / overlay
+    // container / overlay
     <div
       {...props}
       className={cn(
-        // xs - closed: inert, open: overlay
-        // md - closed: inert, open: static spacer
-        'pointer-events-none absolute bottom-0 top-0 z-10 w-80 md:static md:translate-x-0',
+        'pointer-events-none absolute bottom-0 top-0 z-10 w-80',
         side === 'left' ? 'left-0' : 'right-0',
-        open
-          ? overlayCn
-          : side === 'left'
-            ? 'md:absolute md:-translate-x-full'
-            : 'md:absolute md:translate-x-full',
+        open && overlayCn,
         className,
       )}
       ref={forwardedRef}
       onClick={() => onOpenChange && onOpenChange(false)}
     >
-      {/* scrollbar main */}
+      {/* main */}
       <div
         className={cn(
-          'pointer-events-auto absolute bottom-0 top-0 z-50 w-80 border-gold-5 bg-gold-2 transition-transform',
+          'pointer-events-auto absolute bottom-0 top-0 z-50 w-80 border-gold-5 bg-gray-1 transition-transform',
           side === 'left' ? '-translate-x-full border-r' : 'right-0 translate-x-full border-l',
           open && 'translate-x-0',
         )}
