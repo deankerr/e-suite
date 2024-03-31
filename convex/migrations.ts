0dedd2c6eb36af931de7bd12d5124fc1ad8b2d87
Dean Kerr <dean.kerr@gmail.com>
2024-03-31 20:56:50 +1100

    fix threads migration RLS error

--- convex/migrations.ts
+++ convex/migrations.ts
@@ -6,7 +6,7 @@ const fakeId = '' as Id<'users'>
 
 export const threadsFields = internalMutation(async (ctx) => {
   const { roles, prompt } = getNewThreadShape({ userId: fakeId })
-  const threads = await ctx.table('threads')
+  const threads = await ctx.skipRules.table('threads')
 
   await Promise.all(
     threads.map(async (thread) => {
