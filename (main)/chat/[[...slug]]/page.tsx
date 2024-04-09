996560a00a80f2660917daa45c40be1456044de0
Dean Kerr <dean.kerr@gmail.com>
2024-04-10 00:46:56 +1000

    move all type error components out of app temporarily

--- (main)/chat/[[...slug]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Chat } from '../Chat'
-
-import type { Id } from '@/convex/_generated/dataModel'
-
-export default async function ChatPage({ params }: { params: { slug?: [Id<'threads'>] } }) {
-  const threadId = params.slug ? params.slug[0] : undefined
-
-  return <Chat threadId={threadId} />
-}
--- (main)/generate/[[...slug]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Generate } from '../Generate'
-
-import type { Id } from '@/convex/_generated/dataModel'
-
-export default function GeneratePage({ params }: { params: { slug?: [Id<'generations'>] } }) {
-  const generationsId = params.slug ? params.slug[0] : undefined
-
-  return <Generate generationId={generationsId} />
-}
