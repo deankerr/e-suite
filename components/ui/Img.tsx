258d74cf7af91813a53829fdb19ef692134f1871
Dean Kerr <dean.kerr@gmail.com>
2024-04-11 15:43:05 +1000

    move env check into Img component

--- components/ui/Img.tsx
+++ components/ui/Img.tsx
@@ -3,14 +3,14 @@ import NextImage from 'next/image'
 
 import { cn } from '@/lib/utils'
 
-const disableNextOptimization = process.env.NEXT_PUBLIC_IMAGE_UNOPTIMIZED === 'true'
-
 type ImgProps = {} & React.ComponentProps<typeof NextImage>
 
 export const Img = forwardRef<HTMLImageElement, ImgProps>(function Img(
   { className, ...props },
   forwardedRef,
 ) {
+  const disableNextOptimization = process.env.NEXT_PUBLIC_IMAGE_UNOPTIMIZED === 'true'
+
   return (
     <NextImage
       unoptimized={disableNextOptimization}
