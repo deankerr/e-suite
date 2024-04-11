b9af4aa14ed70a491a11e00506706e9cdc929bc8
Dean Kerr <dean.kerr@gmail.com>
2024-04-11 16:20:06 +1000

    fix env boolean check

--- components/ui/Img.tsx
+++ components/ui/Img.tsx
@@ -1,22 +1,10 @@
 import { forwardRef } from 'react'
 import NextImage from 'next/image'
 
-import { cn } from '@/lib/utils'
+const disableNextOptimization = !!process.env.NEXT_PUBLIC_IMAGE_UNOPTIMIZED
 
-type ImgProps = {} & React.ComponentProps<typeof NextImage>
+type ImgProps = React.ComponentProps<typeof NextImage>
 
-export const Img = forwardRef<HTMLImageElement, ImgProps>(function Img(
-  { className, ...props },
-  forwardedRef,
-) {
-  const disableNextOptimization = process.env.NEXT_PUBLIC_IMAGE_UNOPTIMIZED === 'true'
-
-  return (
-    <NextImage
-      unoptimized={disableNextOptimization}
-      {...props}
-      ref={forwardedRef}
-      className={cn('', className)}
-    />
-  )
+export const Img = forwardRef<HTMLImageElement, ImgProps>(function Img({ ...props }, forwardedRef) {
+  return <NextImage unoptimized={disableNextOptimization} {...props} ref={forwardedRef} />
 })
