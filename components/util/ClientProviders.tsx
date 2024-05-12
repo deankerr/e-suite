7ed8bc6b49f291e19823897a08cde91c55f362c0
Dean Kerr <dean.kerr@gmail.com>
2024-05-12 23:07:22 +1000

    jotai devtools tweaks

--- components/util/ClientProviders.tsx
+++ components/util/ClientProviders.tsx
@@ -3,7 +3,7 @@
 import { useAuth } from '@clerk/nextjs'
 import { ConvexReactClient } from 'convex/react'
 import { ConvexProviderWithClerk } from 'convex/react-clerk'
-import { Provider as Jotai } from 'jotai'
+import { createStore, Provider as Jotai } from 'jotai'
 import dynamic from 'next/dynamic'
 
 import { environment } from '@/lib/utils'
@@ -13,10 +13,11 @@ const JotaiDevTools = dynamic(() => import('./JotaiDevTools'), { ssr: false })
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
+  const store = createStore()
   return (
     <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-      <Jotai>
-        {environment === 'dev' && <JotaiDevTools />}
+      <Jotai store={store}>
+        {environment === 'dev' && <JotaiDevTools theme="dark" store={store} />}
         {children}
       </Jotai>
     </ConvexProviderWithClerk>
