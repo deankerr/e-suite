7008513e989d19691b566cb5482b0c9d44f10b81
Dean Kerr <dean.kerr@gmail.com>
2024-06-21 19:23:43 +1000

    add million lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,34 @@
+import MillionLint from '@million/lint';
 /** @type {import('next').NextConfig} */
 
-import bundleAnalyzer from '@next/bundle-analyzer'
-
+import bundleAnalyzer from '@next/bundle-analyzer';
 function getBackendUrl() {
-  const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL
-  if (!convexUrl) throw new Error('NEXT_PUBLIC_CONVEX_URL is not set')
-  return convexUrl.replace('.cloud', '.site')
+  const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL;
+  if (!convexUrl) throw new Error('NEXT_PUBLIC_CONVEX_URL is not set');
+  return convexUrl.replace('.cloud', '.site');
 }
-const backendUrl = getBackendUrl()
-
+const backendUrl = getBackendUrl();
 const nextConfig = {
-  rewrites: async () => [
-    {
-      source: '/i/:slug',
-      destination: `${backendUrl}/i/:slug`,
-    },
-  ],
+  rewrites: async () => [{
+    source: '/i/:slug',
+    destination: `${backendUrl}/i/:slug`
+  }],
   images: {
     loader: 'custom',
-    loaderFile: './lib/image-loader.ts',
+    loaderFile: './lib/image-loader.ts'
   },
   transpilePackages: ['jotai-devtools'],
   experimental: {
-    swcPlugins: [
-      ['@swc-jotai/debug-label', { atomNames: ['customAtom'] }],
-      ['@swc-jotai/react-refresh', { atomNames: ['customAtom'] }],
-    ],
-  },
-}
-
+    swcPlugins: [['@swc-jotai/debug-label', {
+      atomNames: ['customAtom']
+    }], ['@swc-jotai/react-refresh', {
+      atomNames: ['customAtom']
+    }]]
+  }
+};
 const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})
-
-export default withBundleAnalyzer(nextConfig)
+  enabled: process.env.ANALYZE === 'true'
+});
+export default MillionLint.next({
+  rsc: true
+})(withBundleAnalyzer(nextConfig));
