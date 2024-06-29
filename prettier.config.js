38173674a861a0328fd90ef142c9519f82d59187
Dean Kerr <dean.kerr@gmail.com>
2024-06-29 23:33:30 +1000

    upgrade deps

---
+++ prettier.config.js
@@ -0,0 +1,22 @@
+/** @type {import("prettier").Config} */
+
+const config = {
+  semi: false,
+  printWidth: 100,
+  singleQuote: true,
+  plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  importOrder: [
+    '<BUILTIN_MODULES>',
+    '^react$',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^(@/)',
+    '^[.]',
+    '',
+    '<TYPES>',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.0.0',
+}
+
+export default config
