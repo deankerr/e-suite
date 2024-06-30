f6103abfa8d6742778eef31172ceb9d8bf71a9d6
Dean Kerr <dean.kerr@gmail.com>
2024-06-30 22:12:14 +1000

    add basic infinite message scroller

--- components/message-feed/MessageFeed.tsx
+++ components/message-feed/MessageFeed.tsx
@@ -1,52 +1,106 @@
 import { useEffect, useRef, useState } from 'react'
-import { Code } from '@phosphor-icons/react/dist/ssr'
+import { ArrowDown } from '@phosphor-icons/react/dist/ssr'
 import { Button, IconButton } from '@radix-ui/themes'
-import { Virtuoso } from 'react-virtuoso'
+import { useTimeoutEffect } from '@react-hookz/web'
+import { useInView } from 'react-intersection-observer'
 
 import { useChat } from '@/components/chat/ChatProvider'
 import { Message } from '@/components/message/Message'
 import InfiniteScroll from '@/components/ui/InfiniteScroll'
-import { LoadingSpinner } from '@/components/ui/LoadingSpinner'
-import { ScrollContainer } from '@/components/ui/ScrollContainer'
-import { NonSecureAdminRoleOnly } from '@/components/util/NonSecureAdminRoleOnly'
-import { Pre } from '@/components/util/Pre'
 import { useViewerDetails } from '@/lib/queries'
 
 export const MessageFeed = () => {
   const { thread, messages, page, removeMessage } = useChat()
   const { isOwner } = useViewerDetails(thread?.userId)
 
-  const [firstItemIndex, setFirstItemIndex] = useState(1000)
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  // restore scroll position after loading older messages
+  const firstElementId = useRef<string>()
+  const firstElementRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const firstMessageId = messages[0]?._id
+    if (containerRef.current) {
+      if (firstMessageId !== firstElementId.current) {
+        firstElementRef.current?.scrollIntoView({ behavior: 'instant' })
+      }
+    }
+    firstElementId.current = firstMessageId
+  }, [messages])
+
+  const [lastMessageRef, lastMessageInView] = useInView()
+
+  // scroll to new messages if near the bottom
+  const lastElementId = useRef<string>()
+  useEffect(() => {
+    const lastMessageId = messages[messages.length - 1]?._id
+    if (containerRef.current) {
+      if (lastMessageId !== lastElementId.current && lastMessageInView) {
+        containerRef.current.scrollTop = containerRef.current.scrollHeight
+      }
+    }
+
+    lastElementId.current = lastMessageId
+  }, [lastMessageInView, messages])
+
+  const [loaderTimeout, setLoaderTimeout] = useState(true)
+  const [_, reset] = useTimeoutEffect(() => {
+    setLoaderTimeout(false)
+  }, 1000)
+
   const loadMore = () => {
-    if (page.status === 'CanLoadMore') {
+    if (page.status === 'CanLoadMore' && !loaderTimeout) {
       console.log('load')
       page.loadMore(25)
-      setFirstItemIndex(firstItemIndex - 25)
+      reset()
     }
   }
+
+  const scrollToEnd = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight
+    }
+  }
+
   return (
-    <div className="h-full">
-      <Virtuoso
-        style={{ height: '100%' }}
-        data={messages}
-        initialTopMostItemIndex={1000 + messages.length}
-        firstItemIndex={firstItemIndex}
-        startReached={() => {
-          console.log('startReached')
-          loadMore()
-        }}
-        itemContent={(index, message) => (
-          <Message
+    <div className="flex h-full flex-col overflow-hidden">
+      <div ref={containerRef} className="flex-1 overflow-y-auto scroll-smooth">
+        <InfiniteScroll
+          hasMore={page.status === 'CanLoadMore'}
+          isLoading={page.isLoading}
+          next={loadMore}
+        >
+          <Button>{loaderTimeout ? 'timeout' : page.status}</Button>
+        </InfiniteScroll>
+
+        {messages.map((message, index) => (
+          <div
             key={message._id}
-            timeline={index !== messages.length - 1}
-            idx={index}
-            message={message}
-            slug={thread?.slug}
-            showMenu={isOwner}
-            removeMessage={() => void removeMessage({ messageId: message._id })}
-          />
-        )}
-      />
+            ref={
+              firstElementId.current === message._id
+                ? firstElementRef
+                : index === messages.length - 1
+                  ? lastMessageRef
+                  : null
+            }
+          >
+            <Message
+              timeline={index !== messages.length - 1}
+              idx={index}
+              message={message}
+              slug={thread?.slug}
+              showMenu={isOwner}
+              removeMessage={() => void removeMessage({ messageId: message._id })}
+            />
+          </div>
+        ))}
+      </div>
+
+      {!lastMessageInView && (
+        <IconButton variant="soft" className="absolute bottom-3 right-7" onClick={scrollToEnd}>
+          <ArrowDown />
+        </IconButton>
+      )}
     </div>
   )
 }
---
+++ components/message-feed/Virtualized.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import { Virtuoso } from 'react-virtuoso'
+
+import { useChat } from '@/components/chat/ChatProvider'
+import { Message } from '@/components/message/Message'
+import { useViewerDetails } from '@/lib/queries'
+
+export const Virtualized = () => {
+  const { thread, messages, page, removeMessage } = useChat()
+  const { isOwner } = useViewerDetails(thread?.userId)
+  const [firstItemIndex, setFirstItemIndex] = useState(1000)
+  const loadMore = () => {
+    if (page.status === 'CanLoadMore') {
+      console.log('load')
+      page.loadMore(25)
+      setFirstItemIndex(firstItemIndex - 25)
+    }
+  }
+  return (
+    <Virtuoso
+      style={{ height: '100%' }}
+      data={messages}
+      initialTopMostItemIndex={1000 + messages.length}
+      firstItemIndex={firstItemIndex}
+      startReached={() => {
+        console.log('startReached')
+        loadMore()
+      }}
+      itemContent={(index, message) => (
+        <Message
+          key={message._id}
+          timeline={index !== messages.length - 1}
+          idx={index}
+          message={message}
+          slug={thread?.slug}
+          showMenu={isOwner}
+          removeMessage={() => void removeMessage({ messageId: message._id })}
+        />
+      )}
+    />
+  )
+}
