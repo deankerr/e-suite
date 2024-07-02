f2de031f2466b0e0f43c377884c7bb22eefdd220
Dean Kerr <dean.kerr@gmail.com>
2024-07-03 08:18:39 +1000

    fix custom function duplication/types

--- convex/functions.ts
+++ convex/functions.ts
@@ -1,6 +1,5 @@
 import { entsTableFactory, scheduledDeleteFactory } from 'convex-ents'
 import { customCtx, customMutation, customQuery } from 'convex-helpers/server/customFunctions'
-import { zCustomMutation, zCustomQuery } from 'convex-helpers/server/zod'
 import { ConvexError, v } from 'convex/values'
 
 import {
@@ -18,12 +17,13 @@ import type { MutationCtx as BaseMutationCtx, QueryCtx as BaseQueryCtx } from '.
 
 export const scheduledDelete = scheduledDeleteFactory(entDefinitions)
 
-export const authOnlyQuery = zCustomQuery(
-  baseQuery,
-  customCtx(async (baseCtx) => {
-    return await queryCtx(baseCtx)
-  }),
-)
+// * Queries
+export const query = customQuery(baseQuery, {
+  args: { apiKey: v.optional(v.string()) },
+  input: async (baseCtx, { apiKey }) => {
+    return { ctx: await queryCtx(baseCtx, apiKey), args: {} }
+  },
+})
 
 export const internalQuery = customQuery(
   baseInternalQuery,
@@ -32,12 +32,37 @@ export const internalQuery = customQuery(
   }),
 )
 
-export const authOnlyMutation = zCustomMutation(
-  baseMutation,
-  customCtx(async (baseCtx) => {
-    return await mutationCtx(baseCtx)
-  }),
-)
+async function queryCtx(baseCtx: BaseQueryCtx, apiKey?: string) {
+  const ctx = {
+    unsafeDb: baseCtx.db,
+    db: undefined as unknown as typeof baseCtx.db,
+    skipRules: { table: entsTableFactory(baseCtx, entDefinitions) },
+  }
+
+  const authViewerId = await getViewerIdFromAuth({ ...baseCtx, ...ctx })
+  const apiKeyViewerId = await getViewerIdFromApiKey({ ...baseCtx, ...ctx }, apiKey)
+  const viewerId = apiKeyViewerId ?? authViewerId
+
+  const entDefinitionsWithRules = getEntDefinitionsWithRules({ ...ctx, viewerId } as any)
+  const table = entsTableFactory(baseCtx, entDefinitionsWithRules)
+
+  const viewer = async () => (viewerId !== null ? await table('users').get(viewerId) : null)
+  const viewerX = async () => {
+    const ent = await viewer()
+    if (ent === null) throw new ConvexError('Expected authenticated viewer')
+    return ent
+  }
+
+  return { ...ctx, table, viewer, viewerX, viewerId }
+}
+
+// * Mutations
+export const mutation = customMutation(baseMutation, {
+  args: { apiKey: v.optional(v.string()) },
+  input: async (baseCtx, { apiKey }) => {
+    return { ctx: await mutationCtx(baseCtx, apiKey), args: {} }
+  },
+})
 
 export const internalMutation = customMutation(
   baseInternalMutation,
@@ -46,103 +71,29 @@ export const internalMutation = customMutation(
   }),
 )
 
+async function mutationCtx(baseCtx: BaseMutationCtx, apiKey?: string) {
+  const ctx = {
+    unsafeDb: baseCtx.db,
+    db: undefined as unknown as typeof baseCtx.db,
+    skipRules: { table: entsTableFactory(baseCtx, entDefinitions) },
+  }
+
+  const authViewerId = await getViewerIdFromAuth({ ...baseCtx, ...ctx })
+  const apiKeyViewerId = await getViewerIdFromApiKey({ ...baseCtx, ...ctx }, apiKey)
+  const viewerId = apiKeyViewerId ?? authViewerId
+
+  const entDefinitionsWithRules = getEntDefinitionsWithRules({ ...ctx, viewerId } as any)
+  const table = entsTableFactory(baseCtx, entDefinitionsWithRules)
+
+  const viewer = async () => (viewerId !== null ? await table('users').get(viewerId) : null)
+  const viewerX = async () => {
+    const ent = await viewer()
+    if (ent === null) throw new ConvexError('Expected authenticated viewer')
+    return ent
+  }
+
+  return { ...ctx, table, viewer, viewerX, viewerId }
+}
+
 export const action = baseAction
 export const internalAction = baseInternalAction
-
-async function queryCtx(baseCtx: BaseQueryCtx) {
-  const ctx = {
-    unsafeDb: baseCtx.db,
-    db: undefined as unknown as typeof baseCtx.db,
-    skipRules: { table: entsTableFactory(baseCtx, entDefinitions) },
-  }
-
-  const viewerId = await getViewerIdFromAuth({ ...baseCtx, ...ctx })
-
-  const entDefinitionsWithRules = getEntDefinitionsWithRules({ ...ctx, viewerId } as any)
-  const table = entsTableFactory(baseCtx, entDefinitionsWithRules)
-
-  const viewer = async () => (viewerId !== null ? await table('users').get(viewerId) : null)
-  const viewerX = async () => {
-    const ent = await viewer()
-    if (ent === null) throw new ConvexError('Expected authenticated viewer')
-    return ent
-  }
-
-  return { ...ctx, table, viewer, viewerX, viewerId }
-}
-
-async function mutationCtx(baseCtx: BaseMutationCtx) {
-  const ctx = {
-    unsafeDb: baseCtx.db,
-    db: undefined as unknown as typeof baseCtx.db,
-    skipRules: { table: entsTableFactory(baseCtx, entDefinitions) },
-  }
-
-  const viewerId = await getViewerIdFromAuth({ ...baseCtx, ...ctx })
-
-  const entDefinitionsWithRules = getEntDefinitionsWithRules({ ...ctx, viewerId } as any)
-  const table = entsTableFactory(baseCtx, entDefinitionsWithRules)
-
-  const viewer = async () => (viewerId !== null ? await table('users').get(viewerId) : null)
-  const viewerX = async () => {
-    const ent = await viewer()
-    if (ent === null) throw new ConvexError('Expected authenticated viewer')
-    return ent
-  }
-
-  return { ...ctx, table, viewer, viewerX, viewerId }
-}
-
-export const query = customQuery(baseQuery, {
-  args: { apiKey: v.optional(v.string()) },
-  input: async (baseCtx: BaseQueryCtx, { apiKey }: { apiKey?: string }) => {
-    const ctx = {
-      unsafeDb: baseCtx.db,
-      db: undefined as unknown as typeof baseCtx.db,
-      skipRules: { table: entsTableFactory(baseCtx, entDefinitions) },
-    }
-
-    const viewerId =
-      (await getViewerIdFromAuth({ ...baseCtx, ...ctx })) ??
-      (await getViewerIdFromApiKey({ ...baseCtx, ...ctx }, apiKey))
-
-    const entDefinitionsWithRules = getEntDefinitionsWithRules({ ...ctx, viewerId } as any)
-    const table = entsTableFactory(baseCtx, entDefinitionsWithRules)
-
-    const viewer = async () => (viewerId !== null ? await table('users').get(viewerId) : null)
-    const viewerX = async () => {
-      const ent = await viewer()
-      if (ent === null) throw new ConvexError('Expected authenticated viewer')
-      return ent
-    }
-
-    return { ctx: { ...ctx, table, viewer, viewerX, viewerId }, args: {} }
-  },
-})
-
-export const mutation = customMutation(baseMutation, {
-  args: { apiKey: v.optional(v.string()) },
-  input: async (baseCtx, { apiKey }) => {
-    const ctx = {
-      unsafeDb: baseCtx.db,
-      db: undefined as unknown as typeof baseCtx.db,
-      skipRules: { table: entsTableFactory(baseCtx, entDefinitions) },
-    }
-
-    const viewerId =
-      (await getViewerIdFromAuth({ ...baseCtx, ...ctx })) ??
-      (await getViewerIdFromApiKey({ ...baseCtx, ...ctx }, apiKey))
-
-    const entDefinitionsWithRules = getEntDefinitionsWithRules({ ...ctx, viewerId } as any)
-    const table = entsTableFactory(baseCtx, entDefinitionsWithRules)
-
-    const viewer = async () => (viewerId !== null ? await table('users').get(viewerId) : null)
-    const viewerX = async () => {
-      const ent = await viewer()
-      if (ent === null) throw new ConvexError('Expected authenticated viewer')
-      return ent
-    }
-
-    return { ctx: { ...ctx, table, viewer, viewerX, viewerId }, args: {} }
-  },
-})
--- convex/types.ts
+++ convex/types.ts
@@ -1,26 +1,14 @@
-import { entDefinitions } from './schema'
-
 import type { TableNames } from './_generated/dataModel'
-import type { authOnlyMutation, authOnlyQuery } from './functions'
+import type { mutation, query } from './functions'
+import type { entDefinitions } from './schema'
 import type { GenericEnt, GenericEntWriter } from 'convex-ents'
 import type { CustomCtx } from 'convex-helpers/server/customFunctions'
-import type { SystemDataModel } from 'convex/server'
 
-export type QueryCtx = CustomCtx<typeof authOnlyQuery>
-export type MutationCtx = CustomCtx<typeof authOnlyMutation>
+export type QueryCtx = CustomCtx<typeof query>
+export type MutationCtx = CustomCtx<typeof mutation>
 
 export type Ent<TableName extends TableNames> = GenericEnt<typeof entDefinitions, TableName>
 export type EntWriter<TableName extends TableNames> = GenericEntWriter<
   typeof entDefinitions,
   TableName
 >
-
-export type ScheduledFunction = SystemDataModel['_scheduled_functions']['document']
-
-export type CollectionItem<T extends object> = { id: string; name: string } & T
-export type CollectionGroup<T extends object> = {
-  id: string
-  name: string
-  group: CollectionItem<T>[]
-}
-export type Collection<T extends object> = (CollectionItem<T> | CollectionGroup<T>)[]
