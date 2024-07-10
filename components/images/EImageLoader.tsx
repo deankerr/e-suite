4d3ec1922dea8bfa99a937ac6a87cdb45130ea03
Dean Kerr <dean.kerr@gmail.com>
2024-07-10 23:27:04 +1000

    disable custom image loader

--- components/images/EImageLoader.tsx
+++ components/images/EImageLoader.tsx
@@ -7,20 +7,22 @@ import { cn } from '@/lib/utils'
 
 import type { EImage } from '@/convex/types'
 
-const imageLoader = ({ src, width }: { src: string; width: number; quality?: number }) => {
-  return `/i/${src}.webp?w=${width}`
-}
+// const imageLoader = ({ src, width }: { src: string; width: number; quality?: number }) => {
+//   return `/i/${src}.webp?w=${width}`
+// }
 
 export const EImageLoader = forwardRef<
   HTMLImageElement,
   { image: EImage } & Partial<React.ComponentPropsWithoutRef<typeof NextImage>>
 >(({ image, className, ...props }, ref) => {
+  const src = `/i/${image._id}.webp`
+
   return (
     <NextImage
       {...props}
       ref={ref}
-      loader={imageLoader}
-      src={image._id}
+      // loader={imageLoader}
+      src={src}
       alt={props.alt ?? ''}
       placeholder={image.blurDataUrl ? 'blur' : 'empty'}
       blurDataURL={image.blurDataUrl}
