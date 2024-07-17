008485203c8433401eeccd7b048cacd14424aa98
Dean Kerr <dean.kerr@gmail.com>
2024-07-18 00:43:13 +1000

    tweak main layout, bg colors

---
+++ app/dev/lo36/Navigation.tsx
@@ -0,0 +1,43 @@
+import * as Icons from '@phosphor-icons/react/dist/ssr'
+import { Button, IconButton } from '@radix-ui/themes'
+
+import { ChatNavList } from '@/app/dev/lo36/_components/ChatNavList'
+import { CommandShell } from '@/components/command-shell/CommandShell'
+import { UserButtons } from '@/components/layout/UserButtons'
+import { AppLogoName } from '@/components/ui/AppLogoName'
+import { cn } from '@/lib/utils'
+
+export const Navigation = ({ className, ...props }: React.ComponentProps<'div'>) => {
+  return (
+    <div {...props} className={cn('fixed inset-y-0 w-56 px-1.5 py-0.5')}>
+      <nav className="flex h-full flex-col overflow-hidden rounded-md border border-transparent">
+        {/* * logo / menu button * */}
+        <div className="flex h-12 shrink-0 items-center justify-between px-1.5">
+          <AppLogoName className="" />
+
+          <CommandShell>
+            <IconButton variant="ghost" size="1" className="shrink-0">
+              <Icons.List className="size-6" />
+            </IconButton>
+          </CommandShell>
+        </div>
+
+        {/* * compose * */}
+        <div className="flex h-12 shrink-0 justify-center py-2">
+          <Button variant="surface" radius="small">
+            <Icons.Feather weight="light" className="size-5" />
+            Compose
+          </Button>
+        </div>
+
+        {/* * chats * */}
+        <ChatNavList />
+
+        {/* * footer * */}
+        <div className="flex h-12 shrink-0 items-center justify-center border-t border-grayA-3 px-3">
+          <UserButtons />
+        </div>
+      </nav>
+    </div>
+  )
+}
--- components/ui/AppLogoName.tsx
+++ components/ui/AppLogoName.tsx
@@ -3,9 +3,9 @@ import { cn } from '@/lib/utils'
 
 export const AppLogoName = ({ className, ...props }: React.ComponentProps<'div'>) => {
   return (
-    <div {...props} className={cn('flex flex-none items-baseline gap-1', className)}>
-      <Logo className="self-center" />
-      <div className="text-xl font-semibold tracking-tight">e/suite</div>
+    <div {...props} className={cn('flex flex-none items-center gap-1', className)}>
+      <Logo className="-mb-px mr-px" />
+      <div className="text-xl font-semibold leading-5 tracking-tight">e/suite</div>
     </div>
   )
 }
