d4fcc7345e144db8a69dcb2429dffd8bc885d190
Dean Kerr <dean.kerr@gmail.com>
2024-07-19 15:17:30 +1000

    set new thread model/config from Shell

--- app/b/api.ts
+++ app/b/api.ts
@@ -3,9 +3,11 @@ import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 
 import { appConfig } from '@/app/b/config'
+import { useShell } from '@/components/command-shell/useCommandShell'
 import { api } from '@/convex/_generated/api'
 
 export const useCreateThread = () => {
+  const { close } = useShell()
   const router = useRouter()
   const sendCreateThread = useMutation(api.db.threadsB.create)
 
@@ -13,6 +15,7 @@ export const useCreateThread = () => {
     try {
       const result = await sendCreateThread(args)
       router.push(`${appConfig.chatUrl}/${result.slug}`)
+      close()
     } catch (err) {
       console.error(err)
       toast.error('An error occurred while trying to create a new thread.')
