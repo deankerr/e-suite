f4420986e3d61f411d4d5104fac7c55e27a3064a
Dean Kerr <dean.kerr@gmail.com>
2024-07-24 08:44:45 +1000

    move Shell into layout, add cryptic home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,31 @@
+'use client'
+
+import * as Icons from '@phosphor-icons/react/dist/ssr'
+import { IconButton } from '@radix-ui/themes'
+import { Authenticated } from 'convex/react'
+
+import { UserButtons } from '@/components/layout/UserButtons'
+import { useShellActions } from '@/components/shell/hooks'
+import { LogoSvg } from '@/components/ui/LogoSvg'
+
 export default function Page() {
-  return <div></div>
+  const shell = useShellActions()
+
+  return (
+    <div className="flex-col-between h-full p-3">
+      <div className="h-6 w-full">
+        <Authenticated>
+          <IconButton variant="ghost" onClick={() => shell.open()}>
+            <Icons.Terminal />
+          </IconButton>
+        </Authenticated>
+      </div>
+
+      <LogoSvg className="m-auto w-48 text-gray-2" />
+
+      <div className="h-6">
+        <UserButtons />
+      </div>
+    </div>
+  )
 }
