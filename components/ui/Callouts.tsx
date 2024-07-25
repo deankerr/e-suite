4ba519e5d575c43c3f76509e080a1a59851fe6c7
Dean Kerr <dean.kerr@gmail.com>
2024-07-25 16:46:33 +1000

    remove lucide-react

--- components/ui/Callouts.tsx
+++ components/ui/Callouts.tsx
@@ -1,5 +1,5 @@
+import * as Icons from '@phosphor-icons/react/dist/ssr'
 import { Callout } from '@radix-ui/themes'
-import { AlertOctagonIcon } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 
@@ -15,7 +15,7 @@ export const ErrorCallout = ({
   return (
     <Callout.Root color="red" role="alert" className={cn('mx-auto max-w-2xl', className)}>
       <Callout.Icon>
-        <AlertOctagonIcon className="animate-pulse" />
+        <Icons.WarningOctagon className="animate-pulse" />
       </Callout.Icon>
       <Callout.Text className="border-b border-red-6 pb-1">{title}</Callout.Text>
       <Callout.Text className="">{message}</Callout.Text>
