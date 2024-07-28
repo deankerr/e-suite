6dac9c13918f0cb21fd5656d266915d4c39727f0
Dean Kerr <dean.kerr@gmail.com>
2024-07-28 15:05:21 +1000

    add error page scanlines effect

--- app/error.tsx
+++ app/error.tsx
@@ -4,6 +4,8 @@ import { useEffect } from 'react'
 import * as Icons from '@phosphor-icons/react/dist/ssr'
 import { Button } from '@radix-ui/themes'
 
+import { Scanlines } from '@/components/effects/Scanlines'
+
 export default function Error({
   error,
   reset,
@@ -16,21 +18,20 @@ export default function Error({
   }, [error])
 
   return (
-    <div className="flex min-h-dvh">
-      <div className="flex-col-center mx-auto gap-4">
-        <Icons.SmileyXEyes className="size-48 text-red-10 opacity-90" />
-        <h2 className="text-xl text-red-11">Error</h2>
+    <div className="flex-col-center h-dvh gap-4 overflow-hidden p-4">
+      <Scanlines className="animate-scanlinesDown pointer-events-none absolute bottom-0 h-[110%] w-full text-ruby-6 opacity-60" />
 
-        {error.message && (
-          <pre className="max-w-[80%] overflow-x-hidden text-wrap break-all rounded-md border border-ruby-11 bg-red-2 p-4 font-mono text-sm">
-            {error.message}
-          </pre>
-        )}
+      <Icons.SmileyXEyes className="-m-4 size-48 shrink-0 text-red-10 opacity-60" />
 
-        <Button color="red" onClick={() => reset()}>
-          Retry
-        </Button>
+      <h2 className="font-mono text-xl text-red-10">error</h2>
+
+      <div className="w-full max-w-2xl overflow-y-auto overflow-x-hidden text-wrap rounded-md border border-red-11 bg-red-2 p-4 font-mono text-sm">
+        {error?.message}
       </div>
+
+      <Button color="red" variant="solid" onClick={() => reset()}>
+        Retry
+      </Button>
     </div>
   )
 }
