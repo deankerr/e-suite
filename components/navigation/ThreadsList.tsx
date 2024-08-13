8b3c8e75e79406ed032c422b0ffba30dd493f5da
Dean Kerr <dean.kerr@gmail.com>
2024-08-13 23:10:51 +1000

    add NavLink component

--- components/navigation/ThreadsList.tsx
+++ components/navigation/ThreadsList.tsx
@@ -4,9 +4,9 @@ import { useAutoAnimate } from '@formkit/auto-animate/react'
 import * as Icons from '@phosphor-icons/react/dist/ssr'
 import { ScrollArea } from '@radix-ui/themes'
 import { useSetAtom } from 'jotai'
-import Link from 'next/link'
 
 import { sidebarOpenAtom } from '@/components/layout/atoms'
+import { NavLink } from '@/components/navigation/NavLink'
 import { LoadingSpinner } from '@/components/ui/LoadingSpinner'
 import { useThreads } from '@/lib/api'
 import { useSuitePath } from '@/lib/helpers'
@@ -17,14 +17,6 @@ export const ThreadsList = ({ className, ...props }: React.ComponentProps<'div'>
   const toggleSidebar = useSetAtom(sidebarOpenAtom)
   const { threadsList } = useThreads()
   const [containerRef] = useAutoAnimate()
-
-  const linkClassNames = cn(
-    'rounded border border-transparent text-sm font-medium opacity-90',
-    'flex gap-2 py-2 px-2',
-    'hover:bg-gray-2',
-  )
-
-  const activeClassNames = 'bg-gray-2 border-grayA-5 opacity-100'
 
   if (threadsList.length === 0) return null
 
@@ -43,15 +35,15 @@ export const ThreadsList = ({ className, ...props }: React.ComponentProps<'div'>
 
                 if (thread.slug === 'new') return null
                 return (
-                  <Link
+                  <NavLink
                     key={thread._id}
                     href={path.toThread(thread.slug)}
-                    className={cn(linkClassNames, isActive && activeClassNames)}
+                    aria-current={isActive && 'page'}
                     onClick={() => toggleSidebar(false)}
                   >
-                    <IconComponent className="size-5 shrink-0 text-accentA-11" />
+                    <IconComponent size={18} className="text-accentA-11" />
                     <div className="line-clamp-2 select-none">{thread.title ?? 'untitled'}</div>
-                  </Link>
+                  </NavLink>
                 )
               })
             : null}
