b2f04eaeca3d6a54781f48aabe0552173767d8df
Dean Kerr <dean.kerr@gmail.com>
2024-08-20 13:56:21 +1000

    use preloaded query on modal route

--- app/(views)/images/[thread_id]/@modal/(...)image/[image_id]/page.tsx
+++ app/(views)/images/[thread_id]/@modal/(...)image/[image_id]/page.tsx
@@ -1,10 +1,23 @@
-import { Modal } from '@/app/(views)/images/[thread_id]/@modal/(...)image/[image_id]/Modal'
-import { ImagePageLoader1 } from '@/components/_v/ImagePage'
+import { preloadQuery } from 'convex/nextjs'
 
-export default function Page({ params }: { params: { image_id: string; thread_id: string } }) {
+import { Modal } from '@/app/(views)/images/[thread_id]/@modal/(...)image/[image_id]/Modal'
+import { ImagePagePreloaded } from '@/components/_v/ImagePage'
+import { api } from '@/convex/_generated/api'
+
+export default async function Page({
+  params,
+}: {
+  params: { image_id: string; thread_id: string }
+}) {
+  const preloadedImageMessage = await preloadQuery(api.db.images.getImageMessage, {
+    uid: params.image_id,
+  })
   return (
     <Modal closePathname={`/images/${params.thread_id}`}>
-      <ImagePageLoader1 params={params} />
+      <ImagePagePreloaded
+        preloadedImageMessage={preloadedImageMessage}
+        image_id={params.image_id}
+      />
     </Modal>
   )
 }
