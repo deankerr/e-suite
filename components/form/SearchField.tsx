b913976ad38a2434d4fad1e4a158bd4ea2e498c8
Dean Kerr <dean.kerr@gmail.com>
2024-08-23 19:13:48 +1000

    add clear button to SearchField

--- components/form/SearchField.tsx
+++ components/form/SearchField.tsx
@@ -1,12 +1,28 @@
 import * as Icons from '@phosphor-icons/react/dist/ssr'
 
+import { IconButton } from '@/components/ui/Button'
 import { TextField, TextFieldSlot } from '@/components/ui/TextField'
+import { cn } from '@/lib/utils'
 
 export const SearchField = (props: React.ComponentProps<typeof TextField>) => {
   return (
     <TextField {...props}>
       <TextFieldSlot>
         <Icons.MagnifyingGlass className="size-4 shrink-0 opacity-50" />
+      </TextFieldSlot>
+
+      <TextFieldSlot>
+        <IconButton
+          variant="ghost"
+          size="1"
+          aria-label="Clear search"
+          className={cn('invisible', props.value && 'visible')}
+          onClick={() => {
+            props.onValueChange?.('')
+          }}
+        >
+          <Icons.X />
+        </IconButton>
       </TextFieldSlot>
     </TextField>
   )
