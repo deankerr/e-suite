3f4dfe0ecfa474588bdb294402c77e534deb26f2
Dean Kerr <dean.kerr@gmail.com>
2024-08-24 15:02:48 +1000

    fix missing width param in image loader

--- lib/image-loader.ts
+++ lib/image-loader.ts
@@ -1,7 +1,7 @@
 const devImageUrl = process.env.NEXT_PUBLIC_DEV_IMAGE_URL
 
 export default function imageLoader({ src, width }: { src: string; width: number }) {
-  if (src.startsWith('http')) return src
+  if (src.startsWith('http')) return `${src}?w=${width}`
   if (devImageUrl) return `${devImageUrl}${src}?w=${width}&dev=true`
   return `${src}?w=${width}`
 }
