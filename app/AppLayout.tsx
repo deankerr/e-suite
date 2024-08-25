0fe9789b5865ea78cb5b8892e52d0eef956066ec
Dean Kerr <dean.kerr@gmail.com>
2024-08-25 20:34:20 +1000

    add vaul/shadcn Drawer

--- app/AppLayout.tsx
+++ app/AppLayout.tsx
@@ -2,7 +2,11 @@ import { NavRail } from '@/app/NavRail'
 
 export const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex h-dvh bg-midnight md:gap-1.5 md:p-1.5" id="app-layout">
+    <div
+      className="flex h-dvh bg-midnight md:gap-1.5 md:p-1.5"
+      id="app-layout"
+      vaul-drawer-wrapper=""
+    >
       <NavRail />
       {children}
     </div>
