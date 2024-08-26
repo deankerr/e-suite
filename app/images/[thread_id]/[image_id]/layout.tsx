d82632b9e743c95ab4a5c2ae57e87a84c312b55f
Dean Kerr <dean.kerr@gmail.com>
2024-08-26 21:03:14 +1000

    get page metadata in image gallery layout

---
+++ app/images/[thread_id]/[image_id]/layout.tsx
@@ -0,0 +1,21 @@
+import { getConvexSiteUrl } from '@/lib/utils'
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { thread_id: string; image_id: string }
+  modal: React.ReactNode
+}) {
+  const response = await fetch(`${getConvexSiteUrl()}/page?route=image&id=${params.image_id}`)
+  if (!response.ok) return {}
+
+  const data = await response.json()
+  return {
+    title: data.title,
+    description: data.description,
+  }
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>
+}
