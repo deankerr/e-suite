1385fa697ae6f7052ad2bf1ef44c168b5c4ed2e0
Dean Kerr <dean.kerr@gmail.com>
2024-09-02 23:45:33 +1000

    allow inline scripts in iframe

--- app/chat/[thread_id]/page.tsx
+++ app/chat/[thread_id]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { useAtomValue } from 'jotai'
+import * as Icons from '@phosphor-icons/react/dist/ssr'
+import { useAtom } from 'jotai'
 
 import { MessageFeed } from '@/app/chat/[thread_id]/MessageFeed'
 import { htmlTextAtom } from '@/components/artifacts/atoms'
@@ -8,6 +9,7 @@ import { HTMLRenderer } from '@/components/artifacts/HTMLRenderer'
 import { Composer } from '@/components/composer/Composer'
 import { ChatToolbar } from '@/components/threads/ChatToolbar'
 import { Thread } from '@/components/threads/Thread'
+import { IconButton } from '@/components/ui/Button'
 import { Section, SectionHeader } from '@/components/ui/Section'
 import { useThread, useThreadActions } from '@/lib/api'
 
@@ -15,7 +17,7 @@ export default function Page({ params }: { params: { thread_id: string } }) {
   const thread = useThread(params.thread_id)
   const actions = useThreadActions(thread?._id)
 
-  const htmlText = useAtomValue(htmlTextAtom)
+  const [htmlText, setHtmlText] = useAtom(htmlTextAtom)
 
   return (
     <>
@@ -35,7 +37,13 @@ export default function Page({ params }: { params: { thread_id: string } }) {
 
       {htmlText && (
         <Section>
-          <SectionHeader className="px-3">Artifact</SectionHeader>
+          <SectionHeader className="pl-3 pr-1">
+            Artifact
+            <div className="grow" />
+            <IconButton aria-label="Close" variant="ghost" onClick={() => setHtmlText('')}>
+              <Icons.X size={18} />
+            </IconButton>
+          </SectionHeader>
           <div className="grow">
             <HTMLRenderer htmlText={htmlText} />
           </div>
