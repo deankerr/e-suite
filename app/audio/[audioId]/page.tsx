0e9e8d7482d476546e196c23c27cecbc39eff185
Dean Kerr <dean.kerr@gmail.com>
2024-09-06 17:49:30 +1000

    add audio route/get/return

---
+++ app/audio/[audioId]/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { Card } from '@radix-ui/themes'
+import dynamic from 'next/dynamic'
+
+import { useAudio } from '@/app/lib/api/audio'
+import { Panel } from '@/components/ui/Panel'
+import { SkeletonPulse } from '@/components/ui/Skeleton'
+
+const AudioPlayer = dynamic(() => import('@/components/audio/AudioPlayer'), {
+  loading: () => (
+    <Card className="mx-auto aspect-[8/5] w-80">
+      <SkeletonPulse className="absolute inset-0" />
+    </Card>
+  ),
+})
+
+export default function Page({ params }: { params: { audioId: string } }) {
+  const audio = useAudio(params.audioId)
+
+  return (
+    <Panel>
+      <div className="flex-col-center grow">
+        {audio && audio.fileUrl ? (
+          <AudioPlayer
+            key={audio._id}
+            url={audio.fileUrl}
+            titleText={audio.generationData.prompt}
+          />
+        ) : null}
+      </div>
+    </Panel>
+  )
+}
