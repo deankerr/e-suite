6c598ec42ffc048833ba4d3afcd5cc23fd29d23a
Dean Kerr <dean.kerr@gmail.com>
2024-09-07 16:10:09 +1000

    default NavRail to closed

--- components/navigation/NavigationRail.tsx
+++ components/navigation/NavigationRail.tsx
@@ -7,10 +7,7 @@ export const NavigationRail = () => {
   return (
     <div className="z-10 hidden w-11 shrink-0 md:block [&:has(button[data-state=on])]:w-60">
       <Navigation className="w-11 rounded-md border transition-all hover:w-60 [&:has(button[data-state=on])]:w-60">
-        <Toggle.Root
-          defaultPressed
-          className="absolute left-48 top-1 z-10 flex size-10 items-center justify-center rounded text-gray-11 outline-accentA-8 hover:bg-grayA-2 data-[state=on]:text-accent-11"
-        >
+        <Toggle.Root className="absolute left-48 top-1 z-10 flex size-10 items-center justify-center rounded text-gray-11 outline-accentA-8 hover:bg-grayA-2 data-[state=on]:text-accent-11">
           <Icons.SidebarSimple size={24} />
         </Toggle.Root>
       </Navigation>
