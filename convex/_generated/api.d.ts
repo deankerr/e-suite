16740b6937091e8ab1d10c7e3e489ee91731bad7
Dean Kerr <dean.kerr@gmail.com>
2024-09-26 07:42:29 +1000

    update deps

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -1,3 +1,5 @@
+/* prettier-ignore-start */
+
 /* eslint-disable */
 /**
  * Generated `api` utility.
@@ -126,3 +128,5 @@ export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
 >;
+
+/* prettier-ignore-end */
