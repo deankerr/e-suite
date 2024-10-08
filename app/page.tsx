3f5e9e9ec6e0190095c934c10a15afa7b0305836
Dean Kerr <dean.kerr@gmail.com>
2024-10-08 19:17:41 +1100

    add use-client to Loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import { BlankPage } from '@/components/pages/BlankPage'
+import { Panel } from '@/components/ui/Panel'
 
 export default function Page() {
-  return <BlankPage />
+  return (
+    <Panel>
+      <BlankPage />
+    </Panel>
+  )
 }
